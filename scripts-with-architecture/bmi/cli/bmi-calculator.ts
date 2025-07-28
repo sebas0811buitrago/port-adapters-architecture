0cd11225d32ca3c65d048a381f4bdb28d71a19d1
sebas0811buitrago <86497666+sebas0811buitrago@users.noreply.github.com>
2025-07-27 23:03:37 -0500

    renaming

---
+++ scripts-with-architecture/bmi/cli/bmi-calculator.ts
@@ -0,0 +1,17 @@
+// BMi calculators
+
+// Cli applications to calculate and save bmi
+// the cli show option to calculate and save a new bmi or to show previous records
+// option 1: create new bmi records :
+//       users input their weight  (Kg) (from 1 to 200k)
+//       and height (m) (from 1 to 3), the script calculate the bmi,
+//       show the result and shows the weight category
+// option 2: show previous records
+//          show id, weight, height, bmi, and category, and timestamp
+// BMI = weight (kg) / [height (m)]².
+
+//   BMI categories for adults are:
+//  Underweight (<18.5 kg/m²),
+// Normal weight (18.5-24.9 kg/m²),
+// Overweight (25-29.9 kg/m²),
+// and Obese (30 kg/m² or greater).
